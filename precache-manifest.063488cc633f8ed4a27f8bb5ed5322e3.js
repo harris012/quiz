1be986f31e8be2fa3cf03c93a284f8e4265db1a0
harris012 <harissaeed39@yahoo.com>
2021-04-30 08:26:03 +0200

    Updates

---
+++ precache-manifest.063488cc633f8ed4a27f8bb5ed5322e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c530b6840e38c8bae00fd56c0b8dfbb5",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "46ef0aebff84c1f31c70",
+    "url": "/quiz/static/css/main.35088b67.chunk.css"
+  },
+  {
+    "revision": "e819485e4ab338fed58c",
+    "url": "/quiz/static/js/2.c05d0765.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.c05d0765.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46ef0aebff84c1f31c70",
+    "url": "/quiz/static/js/main.691b94d5.chunk.js"
+  },
+  {
+    "revision": "dd19b6e4ede99b84197b",
+    "url": "/quiz/static/js/runtime-main.7a6c2dfa.js"
+  }
+]);
